439fbadd93920f232d114aab2d569150fc85cfc6
Jack Case <jackacase@gmail.com>
2019-03-22 11:47:52 -0400

    Motor directions are now correct

--- source/car_components.cpp
+++ source/car_components.cpp
@@ -8,7 +8,7 @@
 #include "car_components.h"
 
 Motor motor_l { kFTM_Chnl_0, kFTM_Chnl_1, kFTM_Chnl_4, kFTM_Chnl_5 };
-Motor motor_r { kFTM_Chnl_2, kFTM_Chnl_3, kFTM_Chnl_6, kFTM_Chnl_7 };
+Motor motor_r { kFTM_Chnl_3, kFTM_Chnl_2, kFTM_Chnl_6, kFTM_Chnl_7 };
 
 Servo servo {kFTM_Chnl_0, 400, 2000};
 
