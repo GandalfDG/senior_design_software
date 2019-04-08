f403556dea35ce417e1b5da8f841507dcbee8649
Jack Case <jackacase@gmail.com>
2019-04-08 12:20:47 -0400

    these files will hold functions for adjusting the car's target speed and drive motor speed differentials probably other random functionality will drop in here as well

---
+++ source/car_utilities.h
@@ -0,0 +1,13 @@
+/*
+ * car_utilities.h
+ *
+ *  Created on: Apr 8, 2019
+ *      Author: Jack
+ */
+
+#ifndef CAR_UTILITIES_H_
+#define CAR_UTILITIES_H_
+
+#include "car_components.h"
+
+#endif /* CAR_UTILITIES_H_ */
