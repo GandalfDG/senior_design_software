f643d6ba8932142b550f88294c8b7c163f971af0
Jack Case <jackacase@gmail.com>
2019-04-28 11:51:18 -0400

    cleanup, commenting, formatting of Motor class

--- source/car_drivers/Motor.cpp
+++ source/car_drivers/Motor.cpp
@@ -7,7 +7,8 @@
 #include <car_drivers/Motor.h>
 
 Motor::Motor(ftm_chnl_t fwd, ftm_chnl_t rev, ftm_chnl_t enc_a,
-		ftm_chnl_t enc_b) {
+			 ftm_chnl_t enc_b)
+{
 	forward_channel = fwd;
 	reverse_channel = rev;
 	encoder_a_channel = enc_a;
@@ -22,30 +23,33 @@ Motor::Motor(ftm_chnl_t fwd, ftm_chnl_t rev, ftm_chnl_t enc_a,
 
 	pwm_ftm_base = MOTOR_PWM_PERIPHERAL;
 	encoder_ftm_base = ENCODER_TIMER_PERIPHERAL;
-
 }
 
-void Motor::init() {
+void Motor::init()
+{
 	pwm_timer_freq = CLOCK_GetBusClkFreq() / MOTOR_PWM_PERIPHERAL->MOD;
 	encoder_timer_freq = CLOCK_GetBusClkFreq() / ENCODER_TIMER_PERIPHERAL->MOD;
 }
 
-void Motor::set_speed(uint8_t rotation_speed) {
+void Motor::set_speed(uint8_t rotation_speed)
+{
 	assert(rotation_speed >= 0 && rotation_speed <= 100);
 	uint8_t inverse_duty = 100 - rotation_speed;
 
-	if (rotation_direction == FORWARD) {
+	if (rotation_direction == FORWARD)
+	{
 		FTM_UpdatePwmDutycycle(pwm_ftm_base, forward_channel,
-				kFTM_EdgeAlignedPwm, inverse_duty);
+							   kFTM_EdgeAlignedPwm, inverse_duty);
 		FTM_UpdatePwmDutycycle(pwm_ftm_base, reverse_channel,
-				kFTM_EdgeAlignedPwm, 100);
+							   kFTM_EdgeAlignedPwm, 100);
 	}
 
-	else if (rotation_direction == REVERSE) {
+	else if (rotation_direction == REVERSE)
+	{
 		FTM_UpdatePwmDutycycle(pwm_ftm_base, reverse_channel,
-				kFTM_EdgeAlignedPwm, inverse_duty);
+							   kFTM_EdgeAlignedPwm, inverse_duty);
 		FTM_UpdatePwmDutycycle(pwm_ftm_base, forward_channel,
-				kFTM_EdgeAlignedPwm, 100);
+							   kFTM_EdgeAlignedPwm, 100);
 	}
 
 	FTM_SetSoftwareTrigger(pwm_ftm_base, true);
@@ -53,58 +57,72 @@ void Motor::set_speed(uint8_t rotation_speed) {
 	Motor::rotation_speed = rotation_speed;
 }
 
-void Motor::set_direction(direction dir) {
+void Motor::set_direction(direction dir)
+{
 	rotation_direction = dir;
 	set_speed(rotation_speed);
 }
 
-Motor::direction Motor::getRotationDirection() const {
+Motor::direction Motor::getRotationDirection() const
+{
 	return rotation_direction;
 }
 
-uint8_t Motor::getRotationSpeed() const {
+uint8_t Motor::getRotationSpeed() const
+{
 	return rotation_speed;
 }
 
-void Motor::stop(void) {
+void Motor::stop(void)
+{
 	FTM_UpdatePwmDutycycle(pwm_ftm_base, forward_channel,
-			(ftm_pwm_mode_t) Motor_PWM_config.pwmSyncMode, 0);
+						   (ftm_pwm_mode_t)Motor_PWM_config.pwmSyncMode, 0);
 	FTM_UpdatePwmDutycycle(pwm_ftm_base, reverse_channel,
-			(ftm_pwm_mode_t) Motor_PWM_config.pwmSyncMode, 0);
+						   (ftm_pwm_mode_t)Motor_PWM_config.pwmSyncMode, 0);
 	rotation_speed = 0;
 }
 
-uint16_t Motor::getPhysicalSpeed() {
+uint16_t Motor::getPhysicalSpeed()
+{
 	return physical_speed;
 }
 
 //this is called from an ISR with the latest capture value
-void Motor::update_encoder_period(uint32_t captured) {
+void Motor::update_encoder_period(uint32_t captured)
+{
 	encoder_a_prev = encoder_a_curr;
 	encoder_a_curr = captured;
-	if (encoder_a_prev <= encoder_a_curr) {
+	if (encoder_a_prev <= encoder_a_curr)
+	{
 		raw_period = encoder_a_curr - encoder_a_prev;
-	} else if (encoder_a_prev > encoder_a_curr) {
+	}
+	else if (encoder_a_prev > encoder_a_curr)
+	{
 		raw_period = encoder_a_prev - encoder_a_curr;
-	} else {
+	}
+	else
+	{
 		raw_period = 0;
 	}
 	//TODO call this in a motor driving task, not every interrupt
 	//period_to_rpm();
 }
 
-void Motor::motor_test(void) {
+void Motor::motor_test(void)
+{
 	vTaskSuspend(NULL);
 	set_direction(FORWARD);
 
 	// drive motors from 0 to full forward
-	for (int i = 0; i <= 100; i++) {
+	for (int i = 0; i <= 100; i++)
+	{
 		set_speed(i);
 		vTaskDelay(pdMS_TO_TICKS(MOTOR_TEST_PERIOD));
 	}
 
 	// from full forward to 0
-	for (int i = 100; i >= 0; i--) {
+	for (int i = 100; i >= 0; i--)
+	{
 		set_speed(i);
 		vTaskDelay(pdMS_TO_TICKS(MOTOR_TEST_PERIOD));
 	}
@@ -112,18 +130,21 @@ void Motor::motor_test(void) {
 	set_direction(REVERSE);
 
 	// drive motors from 0 to full reverse
-	for (int i = 0; i <= 100; i++) {
+	for (int i = 0; i <= 100; i++)
+	{
 		set_speed(i);
 		vTaskDelay(pdMS_TO_TICKS(MOTOR_TEST_PERIOD));
 	}
 
 	// from full reverse to 0
-	for (int i = 100; i >= 0; i--) {
+	for (int i = 100; i >= 0; i--)
+	{
 		set_speed(i);
 		vTaskDelay(pdMS_TO_TICKS(MOTOR_TEST_PERIOD));
 	}
 }
 
-void Motor::period_to_rpm(void) {
+void Motor::period_to_rpm(void)
+{
 	physical_speed = (raw_period * 12 * 60) / encoder_timer_freq;
 }
--- source/car_drivers/Motor.h
+++ source/car_drivers/Motor.h
@@ -1,6 +1,11 @@
 /*
  * motor.h
- *
+ * 
+ * This class contains code for driving a motor via an h-bridge with PWM-
+ * controlled speed, as well as for reading motor speed with an encoder.
+ * 
+ * For and example of Motor object construction, refer to car_components.cpp
+ * 
  *  Created on: Jan 4, 2019
  *      Author: Jack
  */
@@ -16,24 +21,29 @@
 
 #define MOTOR_TEST_PERIOD (100) //milliseconds
 
-class Motor {
-public:
-	enum direction {
+class Motor
+{
+  public:
+	enum direction
+	{
 		FORWARD,
 		REVERSE
 	};
 
-	FTM_Type* pwm_ftm_base;
-	FTM_Type* encoder_ftm_base;
+	//the peripheral base addresses for the h-bridge and encoders
+	FTM_Type *pwm_ftm_base;
+	FTM_Type *encoder_ftm_base;
 
 	TaskHandle_t test_task_handle;
 
 	// the channels corresponding to driving the motor forward or in reverse
 	ftm_chnl_t forward_channel, reverse_channel;
+	// the channels corresponding to each encoder
 	ftm_chnl_t encoder_a_channel, encoder_b_channel;
 
 	Motor(ftm_chnl_t fwd, ftm_chnl_t rev, ftm_chnl_t enc_a, ftm_chnl_t enc_b);
 
+	// this should be called after peripheral initialization to get some important values
 	void init();
 
 	void set_speed(uint8_t rotation_speed);
@@ -43,13 +53,13 @@ public:
 	uint8_t getRotationSpeed() const;
 
 	static void ENCODER_TIMER_IRQHANDLER();
-	
+
 	void update_encoder_period(uint32_t captured);
 
 	void motor_test(void);
 	uint16_t getPhysicalSpeed();
 
-private:
+  private:
 	void period_to_rpm(void);
 	// internal values for determining the actual rotation speed of the motor
 	uint16_t encoder_a_prev, encoder_b_prev;
@@ -62,7 +72,5 @@ private:
 	uint16_t raw_period;
 	uint16_t physical_speed;
 };
-
-
 
 #endif /* CAR_DRIVERS_MOTOR_H_ */
