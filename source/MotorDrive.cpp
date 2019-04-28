c903dae447954532b52f06aaa1d204dec6559141
Jack Case <jackacase@gmail.com>
2019-04-28 12:02:53 -0400

    cleanup, commenting, formatting of MotorDrive class

--- source/MotorDrive.cpp
+++ source/MotorDrive.cpp
@@ -7,34 +7,39 @@
 
 #include <MotorDrive.h>
 
-void MotorDrive::update_from_camera(int centerline) {
+void MotorDrive::update_from_camera(int centerline)
+{
 	int diff_amt = 0;
 	//if on centerline, increase speed towards top speed
-	if(centerline < CENTER + CENTER_BUFFER && centerline > CENTER - CENTER_BUFFER) {
+	if (centerline < CENTER + CENTER_BUFFER && centerline > CENTER - CENTER_BUFFER)
+	{
 		current_speed += current_speed + 1 <= TOP_SPEED ? 1 : 0;
 		set_motors(current_speed);
 	}
-	else if(centerline >= CENTER + CENTER_BUFFER) {
+	else if (centerline >= CENTER + CENTER_BUFFER)
+	{
 		current_speed = MIN_SPEED;
 		diff_amt = centerline - CENTER;
 		set_motors(current_speed, current_speed + diff_amt);
 	}
-	else {
+	else
+	{
 		current_speed = MIN_SPEED;
 		diff_amt = CENTER - centerline;
 		set_motors(current_speed + diff_amt, current_speed);
 	}
-
 }
 
-void MotorDrive::set_motors(int duty) {
+void MotorDrive::set_motors(int duty)
+{
 	motor_l.set_direction(Motor::FORWARD);
 	motor_r.set_direction(Motor::FORWARD);
 	motor_l.set_speed(duty);
 	motor_r.set_speed(duty);
 }
 
-void MotorDrive::set_motors(int duty_l, int duty_r) {
+void MotorDrive::set_motors(int duty_l, int duty_r)
+{
 	motor_l.set_direction(Motor::FORWARD);
 	motor_r.set_direction(Motor::FORWARD);
 	motor_l.set_speed(duty_l);
--- source/MotorDrive.h
+++ source/MotorDrive.h
@@ -1,5 +1,13 @@
 /*
  * MotorDrive.h
+ * 
+ * This class facilitates driving both rear motors in tandem.
+ * It provides functions for setting their speed simultaneously,
+ * as well as adjusting speed from the camera input, namely 
+ * speeding up on straightaways, slowing down in corners, and 
+ * differential speed of the drive motors when turning.
+ * 
+ * For an example of MotorDrive object construction, refer to car_components.cpp
  *
  *  Created on: Apr 23, 2019
  *      Author: Jack
@@ -12,18 +20,19 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#define TOP_SPEED (50)
+#define TOP_SPEED (60)
 #define MIN_SPEED (50)
 
 #define CENTER (64)
 #define CENTER_BUFFER (8)
 
-class MotorDrive {
-public:
+class MotorDrive
+{
+  public:
 	TaskHandle_t task_handle;
 	unsigned int current_speed;
 	Motor &motor_l, &motor_r;
-	MotorDrive(Motor &motor_l, Motor &motor_r): motor_l{motor_l}, motor_r{motor_r} {};
+	MotorDrive(Motor &motor_l, Motor &motor_r) : motor_l{motor_l}, motor_r{motor_r} {};
 
 	void update_from_camera(int centerline);
 	void set_motors(int duty);
