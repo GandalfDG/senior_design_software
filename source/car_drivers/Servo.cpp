9ab7bdaf41ae75c60d72e54c1ef7e846dc950ec6
Jack Case <jackacase@gmail.com>
2019-04-28 11:59:28 -0400

    cleanup, commenting, formatting of Servo class

--- source/car_drivers/Servo.cpp
+++ source/car_drivers/Servo.cpp
@@ -7,12 +7,14 @@
 
 #include <car_drivers/Servo.h>
 
-Servo::Servo() {
+Servo::Servo()
+{
 	Servo(kFTM_Chnl_0, 400, 2000);
 }
 
 Servo::Servo(ftm_chnl_t servo_channel, uint16_t left_width,
-		uint16_t right_width) {
+			 uint16_t right_width)
+{
 	servo_channel = servo_channel;
 	left_pulse_width = left_width;
 	right_pulse_width = right_width;
@@ -20,8 +22,11 @@ Servo::Servo(ftm_chnl_t servo_channel, uint16_t left_width,
 	current_pulse_width = center_pulse_width;
 }
 
-void Servo::set_position(uint16_t pos) {
+void Servo::set_position(uint16_t pos)
+{
 	uint16_t cnv, mod;
+	pos = pos >= left_pulse_width ? pos : left_pulse_width;
+	pos = pos <= right_pulse_width ? pos : right_pulse_width;
 	mod = pwm_ftm_base->MOD;
 
 	cnv = (mod * pos) / SERVO_PWM_PERIOD;
@@ -32,7 +37,8 @@ void Servo::set_position(uint16_t pos) {
 	current_pulse_width = pos;
 }
 
-void Servo::servo_test(void) {
+void Servo::servo_test(void)
+{
 	vTaskSuspend(NULL);
 
 	// start at center
@@ -40,7 +46,8 @@ void Servo::servo_test(void) {
 
 	// move to full left
 	for (int i = center_pulse_width; i >= left_pulse_width; i -=
-	SERVO_TEST_STEP) {
+															SERVO_TEST_STEP)
+	{
 		set_position(i);
 		vTaskDelay(pdMS_TO_TICKS(SERVO_TEST_PERIOD));
 	}
@@ -48,30 +55,34 @@ void Servo::servo_test(void) {
 	// move to full right
 
 	for (int i = left_pulse_width; i <= right_pulse_width; i +=
-	SERVO_TEST_STEP) {
+														   SERVO_TEST_STEP)
+	{
 		set_position(i);
 		vTaskDelay(pdMS_TO_TICKS(SERVO_TEST_PERIOD));
 	}
 
 	// move back to center
 	for (int i = right_pulse_width; i >= center_pulse_width; i -=
-	SERVO_TEST_STEP) {
+															 SERVO_TEST_STEP)
+	{
 		set_position(i);
 		vTaskDelay(pdMS_TO_TICKS(SERVO_TEST_PERIOD));
 	}
-
 }
 
-void Servo::position_from_camera(int centerline) {
-int exp_centerline;
-if(centerline >= 64) {
-	exp_centerline = (int)(centerline + pow(EXPVAL, centerline - 64));
-	exp_centerline = exp_centerline <= NUM_PIXELS ? exp_centerline : NUM_PIXELS;
-}
-else {
-	exp_centerline = (int)(centerline - pow(EXPVAL, 64 - centerline));
-	exp_centerline = exp_centerline >= 0 ? exp_centerline : 0;
-}
-int position = (right_pulse_width - left_pulse_width) * (((float)NUM_PIXELS - (float)exp_centerline) / (float)NUM_PIXELS) + left_pulse_width;
+void Servo::position_from_camera(int centerline)
+{
+	int exp_centerline;
+	if (centerline >= 64)
+	{
+		exp_centerline = (int)(centerline + pow(EXPVAL, centerline - 64));
+		exp_centerline = exp_centerline <= NUM_PIXELS ? exp_centerline : NUM_PIXELS;
+	}
+	else
+	{
+		exp_centerline = (int)(centerline - pow(EXPVAL, 64 - centerline));
+		exp_centerline = exp_centerline >= 0 ? exp_centerline : 0;
+	}
+	int position = (right_pulse_width - left_pulse_width) * (((float)NUM_PIXELS - (float)exp_centerline) / (float)NUM_PIXELS) + left_pulse_width;
 	set_position(position);
 }
--- source/car_drivers/Servo.h
+++ source/car_drivers/Servo.h
@@ -1,6 +1,10 @@
 /*
  * Servo.h
  *
+ * This class contains code for driving a servo motor via a PWM signal.
+ * 
+ * For an example of servo object construction, refer to car_components.cpp
+ * 
  *  Created on: Jan 6, 2019
  *      Author: Jack
  */
@@ -21,31 +25,32 @@
 #define NUM_PIXELS (128)
 #define EXPVAL (1.2f)
 
-class Servo {
-public:
-
+class Servo
+{
+  public:
 	TaskHandle_t test_task_handle;
 	TaskHandle_t task_handle;
 
-	FTM_Type* pwm_ftm_base = SERVO_PWM_PERIPHERAL;
+	FTM_Type *pwm_ftm_base = SERVO_PWM_PERIPHERAL;
 
 	// pulse widths in microseconds
 	uint16_t center_pulse_width, left_pulse_width, right_pulse_width;
 	uint16_t current_pulse_width;
 
-
 	Servo();
 	Servo(ftm_chnl_t servo_channel, uint16_t left_width, uint16_t right_width);
 
+	//sets the pulse width to the specified number of microseconds
 	void set_position(uint16_t pos);
+
+	//moves the servo back and forth for testing
 	void servo_test(void);
 
+	//sets the servo position based on the provided current centerline value
 	void position_from_camera(int centerline);
 
-private:
+  private:
 	ftm_chnl_t servo_channel;
-
-
 };
 
 #endif /* CAR_DRIVERS_SERVO_H_ */
