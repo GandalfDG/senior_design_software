1f9976ef627dde09a808ccfa0e7217b37244ee45
Jack Case <jackacase@gmail.com>
2019-04-28 12:12:44 -0400

    commenting, formatting of interrupt handlers

--- source/interrupt_handlers.cpp
+++ source/interrupt_handlers.cpp
@@ -7,35 +7,36 @@
 
 #include "interrupt_handlers.h"
 
-void FTM3_IRQHandler(void) {
+void FTM3_IRQHandler(void)
+{
 	//TODO handle the timer overflow case, speed might temporarily be wrong if the timer overflows
 	uint32_t capture_val_l = 0;
 	uint32_t capture_val_r = 0;
 	// read counter value from triggered channel(s)
-	uint32_t encoder_l_flags = FTM_GetStatusFlags(motor_l.encoder_ftm_base)
-			& (1 << motor_l.encoder_a_channel);
-	uint32_t encoder_r_flags = FTM_GetStatusFlags(motor_r.encoder_ftm_base)
-			& (1 << motor_r.encoder_a_channel);
+	uint32_t encoder_l_flags = FTM_GetStatusFlags(motor_l.encoder_ftm_base) & (1 << motor_l.encoder_a_channel);
+	uint32_t encoder_r_flags = FTM_GetStatusFlags(motor_r.encoder_ftm_base) & (1 << motor_r.encoder_a_channel);
 
-	if (encoder_l_flags) {
+	if (encoder_l_flags)
+	{
 		capture_val_l =
-				motor_l.encoder_ftm_base->CONTROLS[motor_l.encoder_a_channel].CnV;
+			motor_l.encoder_ftm_base->CONTROLS[motor_l.encoder_a_channel].CnV;
 		FTM_ClearStatusFlags(motor_l.encoder_ftm_base,
-				1u << motor_l.encoder_a_channel);
+							 1u << motor_l.encoder_a_channel);
 		motor_l.update_encoder_period(capture_val_l);
 	}
 
-	if (encoder_r_flags) {
+	if (encoder_r_flags)
+	{
 		capture_val_r =
-				motor_r.encoder_ftm_base->CONTROLS[motor_r.encoder_a_channel].CnV;
+			motor_r.encoder_ftm_base->CONTROLS[motor_r.encoder_a_channel].CnV;
 		FTM_ClearStatusFlags(motor_r.encoder_ftm_base,
-				1u << motor_r.encoder_a_channel);
+							 1u << motor_r.encoder_a_channel);
 		motor_r.update_encoder_period(capture_val_r);
 	}
 }
 
-//TODO clean this up a bit
-void FTM1_IRQHandler(void) {
+void FTM1_IRQHandler(void)
+{
 	BaseType_t woken = pdFALSE;
 	int16_t pixel_num = 0;
 	FTM_ClearStatusFlags(camera.ftm_base, 1u << camera.ftm_channel);
@@ -45,27 +46,35 @@ void FTM1_IRQHandler(void) {
 	GPIO_PortToggle(camera.gpio_base, 1u << camera.clk_pin);
 
 	pixel_num = camera.current_pixel;
-	if(pixel_num >=2 && pixel_num < 256) {
-		camera.line_buffer[pixel_num/2] = camera.adc_value;
+	if (pixel_num >= 2 && pixel_num < 256)
+	{
+		camera.line_buffer[pixel_num / 2] = camera.adc_value;
 		camera.current_pixel++;
 	}
 	//generate the SI pulse
-	else if(pixel_num < 2) {
-		if(pixel_num == -1) {
+	else if (pixel_num < 2)
+	{
+		if (pixel_num == -1)
+		{
 			GPIO_PinWrite(camera.gpio_base, camera.si_pin, 1);
 		}
-		else if(pixel_num == 1) {
+		else if (pixel_num == 1)
+		{
 			GPIO_PinWrite(camera.gpio_base, camera.si_pin, 0);
 			camera.line_buffer[0] = camera.adc_value;
 		}
 		camera.current_pixel++;
 	}
-	else {
+	else
+	{
 		//a full line has been read from the camera - notify the processing task
 		GPIO_PinWrite(camera.gpio_base, camera.clk_pin, 0);
 		camera.current_pixel = -2;
 
-		if(camera.task_handle) {
+		if (camera.task_handle)
+		{
+			//this directly notifies the camera task, acting as a binary semaphore
+			//based on the period of the camera task, some lines may be dropped
 			vTaskNotifyGiveFromISR(camera.task_handle, &woken);
 			portYIELD_FROM_ISR(woken);
 		}
@@ -74,19 +83,16 @@ void FTM1_IRQHandler(void) {
 	}
 }
 
-void PIT0_IRQHandler(void) {
+void PIT0_IRQHandler(void)
+{
 	//clear PIT interrupt flag
 	PIT_ClearStatusFlags(camera.pit_base, camera.pit_channel, kPIT_TimerFlag);
-
-	// Setting mod resets the FTM counter
-	//TODO Don't think this is necessary...
-    //FTM2_MOD = DEFAULT_SYSTEM_CLOCK / 100000;
 
 	//enable camera FTM interrupts to start capture
 	FTM_EnableInterrupts(camera.ftm_base, 1u << camera.ftm_channel);
 }
 
-void ADC0_IRQHandler(void) {
+void ADC0_IRQHandler(void)
+{
 	camera.adc_value = ADC16_GetChannelConversionValue(camera.adc_base, 0);
 }
-
--- source/interrupt_handlers.h
+++ source/interrupt_handlers.h
@@ -1,5 +1,7 @@
 /*
  * interrupt_handlers.h
+ * 
+ * This file contains the interrupt handlers for various peripherals.
  *
  *  Created on: Feb 10, 2019
  *      Author: Jack
@@ -15,13 +17,13 @@
 #include "peripherals.h"
 #include "car_components.h"
 
-extern "C" {
+//extern C is needed to allow C to see C++ functions
+extern "C"
+{
 	void FTM3_IRQHandler(void);
 	void PIT0_IRQHandler(void);
 	void FTM1_IRQHandler(void);
 	void ADC0_IRQHandler(void);
 }
-
-
 
 #endif /* INTERRUPT_HANDLERS_H_ */
