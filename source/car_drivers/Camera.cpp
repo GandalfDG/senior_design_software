49034024da746ff29b4e1a09ce96d4274c0be65c
Jack Case <jackacase@gmail.com>
2019-04-28 11:46:02 -0400

    cleanup, commenting, formatting of Camera class

--- source/car_drivers/Camera.cpp
+++ source/car_drivers/Camera.cpp
@@ -1,13 +1,14 @@
 /*
  * Camera.cpp
- *
+ * 
  *  Created on: Feb 21, 2019
  *      Author: Jack
  */
 
 #include <car_drivers/Camera.h>
 
-void Camera::init() {
+void Camera::init()
+{
 	//set gpio pins as output
 	gpio_pin_config_t gpioconfig;
 	gpioconfig.pinDirection = kGPIO_DigitalOutput;
@@ -25,44 +26,27 @@ void Camera::init() {
 	//initialize camera data struct
 	camera_data.center = NUM_PIXELS / 2;
 	camera_data.prev_center = camera_data.center;
-
 }
 
-void Camera::process(void) {
+void Camera::process(void)
+{
 	uint16_t current_line[NUM_PIXELS];
-//	uint16_t previous_line[NUM_PIXELS];
 	uint16_t *temp;
 
-	//load the previous value on the first run
-//	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-//	memcpy((void*) previous_line, (const void*) line_buffer,
-//			NUM_PIXELS * sizeof(uint16_t));
-
-//wait for the line buffer to be full
+	//wait for a line of pixels to be available
 	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
 	//copy the current line into a local buffer
-	memcpy((void*) current_line, (const void*) line_buffer,
-	NUM_PIXELS * sizeof(uint16_t));
+	memcpy((void *)current_line, (const void *)line_buffer,
+		   NUM_PIXELS * sizeof(uint16_t));
 
-	//find edges
+	//process the line
 	find_edges(current_line);
 	calculate_centerline(&camera_data);
-
-	//move current line to previous line
-	//TODO optimize this by swapping pointers around instead of copying all the time
-//		memcpy((void*) previous_line, (const void*) current_line,
-//				NUM_PIXELS * sizeof(uint16_t));
-
-	//pass data to relevant tasks (motor, servo)
-
 }
 
-void Camera::filter(void) {
-
-}
-
-void Camera::calibrate(void) {
+void Camera::calibrate(void)
+{
 	uint16_t average[NUM_PIXELS];
 	uint16_t current[NUM_PIXELS];
 	//initialize the average
@@ -72,91 +56,111 @@ void Camera::calibrate(void) {
 	calibration.max = 0;
 	calibration.min = UINT16_MAX;
 
-	memcpy((void*) average, (const void*) line_buffer,
-	NUM_PIXELS * sizeof(uint16_t));
+	memcpy((void *)average, (const void *)line_buffer,
+		   NUM_PIXELS * sizeof(uint16_t));
 
 	//average some number of camera readings
-	for (int i = 0; i < CALIBRATION_COUNT; i++) {
+	for (int i = 0; i < CALIBRATION_COUNT; i++)
+	{
 		//wait for a camera reading
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-		memcpy((void*) current, (const void*) line_buffer,
-		NUM_PIXELS * sizeof(uint16_t));
+		memcpy((void *)current, (const void *)line_buffer,
+			   NUM_PIXELS * sizeof(uint16_t));
 
-		for (int j = 0; j < NUM_PIXELS; j++) {
+		for (int j = 0; j < NUM_PIXELS; j++)
+		{
 			average[j] = (average[j] + current[j]) / 2;
 		}
-
 	}
 
 	//find max, min, and calculate a threshold value
-	for (int i = 0; i < NUM_PIXELS; i++) {
-		if (average[i] > calibration.max) {
+	for (int i = 0; i < NUM_PIXELS; i++)
+	{
+		if (average[i] > calibration.max)
+		{
 			calibration.max = average[i];
 		}
-		if (average[i] < calibration.min) {
+		if (average[i] < calibration.min)
+		{
 			calibration.min = average[i];
 		}
 	}
 
-	//TODO a better threshold might be some percentage other than 50%
-	calibration.rising_threshold = (calibration.max + calibration.min)
-			/ 2+ THRESHOLD_WIDTH;
-	calibration.falling_threshold = (calibration.max + calibration.min)
-			/ 2- THRESHOLD_WIDTH;
-
+	//separate rising and falling thresholds give better results than a 50% threshold
+	calibration.rising_threshold = (calibration.max + calibration.min) / 2 + THRESHOLD_WIDTH;
+	calibration.falling_threshold = (calibration.max + calibration.min) / 2 - THRESHOLD_WIDTH;
 }
 
-void Camera::find_edges(uint16_t* camline) {
-//	camera_data.right_edge_outer = find_edge_between(0, camera_data.prev_center + FUZZY_WIDTH, camline, FALLING, false);
+void Camera::find_edges(uint16_t *camline)
+{
 	camera_data.right_edge_inner = find_edge_between(0, NUM_PIXELS / 2, camline, FALLING, true);
-//	camera_data.left_edge_outer = find_edge_between
 	camera_data.left_edge_inner = find_edge_between(NUM_PIXELS / 2, NUM_PIXELS, camline, FALLING, false);
 }
 
 uint8_t Camera::find_edge_between(uint8_t lower_bound, uint8_t upper_bound,
-		uint16_t *camline, edge_polarity pol, bool reverse) {
+								  uint16_t *camline, edge_polarity pol, bool reverse)
+{
 	int8_t edge = -1;
-	if (!reverse) {
+	if (!reverse)
+	{
 		for (int i = lower_bound >= 0 ? lower_bound : 0;
-				i < (upper_bound <= NUM_PIXELS ? upper_bound : NUM_PIXELS)
-						&& edge == -1; i++) {
-			if (pol == RISING) {
-				if (camline[i] > calibration.rising_threshold) {
-					edge = i;
-				}
-			} else {
-				if (camline[i] < calibration.falling_threshold) {
+			 i < (upper_bound <= NUM_PIXELS ? upper_bound : NUM_PIXELS) && edge == -1; i++)
+		{
+			if (pol == RISING)
+			{
+				if (camline[i] > calibration.rising_threshold)
+				{
 					edge = i;
 				}
 			}
-		}
-	} else {
-		for (int i = upper_bound < NUM_PIXELS ? upper_bound : NUM_PIXELS - 1;
-				i > (lower_bound >= 0 ? lower_bound : 0) && edge == -1; i--) {
-			if (pol == RISING) {
-				if (camline[i] > calibration.rising_threshold) {
-					edge = i;
-				}
-			} else {
-				if (camline[i] < calibration.falling_threshold) {
+			else
+			{
+				if (camline[i] < calibration.falling_threshold)
+				{
 					edge = i;
 				}
 			}
 		}
 	}
-	return (uint8_t) edge >= 0 ? edge : 0;
+	else
+	{
+		for (int i = upper_bound < NUM_PIXELS ? upper_bound : NUM_PIXELS - 1;
+			 i > (lower_bound >= 0 ? lower_bound : 0) && edge == -1; i--)
+		{
+			if (pol == RISING)
+			{
+				if (camline[i] > calibration.rising_threshold)
+				{
+					edge = i;
+				}
+			}
+			else
+			{
+				if (camline[i] < calibration.falling_threshold)
+				{
+					edge = i;
+				}
+			}
+		}
+	}
+	return (uint8_t)edge >= 0 ? edge : 0;
 }
 
-uint8_t Camera::calculate_centerline(struct data* camdata) {
-	if (camdata->left_edge_inner == camdata->right_edge_inner) {
+uint8_t Camera::calculate_centerline(struct data *camdata)
+{
+	if (camdata->left_edge_inner == camdata->right_edge_inner)
+	{
 		camdata->center = camdata->prev_center;
 	}
-	else {
+	else
+	{
 		int temp_center = (camdata->left_edge_inner + camdata->right_edge_inner) / 2;
-		if(abs(temp_center - camdata->prev_center) > BAD_CENTER_THRESHOLD) {
+		if (abs(temp_center - camdata->prev_center) > BAD_CENTER_THRESHOLD)
+		{
 			camdata->center = camdata->prev_center;
 		}
-		else {
+		else
+		{
 			camdata->center = temp_center;
 		}
 	}
--- source/car_drivers/Camera.h
+++ source/car_drivers/Camera.h
@@ -1,6 +1,16 @@
 /*
  * Camera.h
  *
+ * This file contains code for processing input from the linescan camera.
+ * The actual code that drives and reads values from the camera is located in
+ * interrupt_handlers.cpp 
+ * 
+ * The interrupt handler notifies the camera RTOS task when a full line has been
+ * read, unblocking the process function and copying that full line into a local
+ * buffer.
+ * 
+ * For an example of camera object construction, refer to car_components.cpp
+ * 
  *  Created on: Feb 21, 2019
  *      Author: Jack
  */
@@ -18,39 +28,39 @@
 
 #include <stdint.h>
 
-#define NUM_PIXELS (128)			//the number of camera pixels
-#define CALIBRATION_COUNT (256)		//the number of camera readings to average for calibration
+#define NUM_PIXELS (128)		//the number of camera pixels
+#define CALIBRATION_COUNT (256) //the number of camera readings to average for calibration
 
-#define FUZZY_WIDTH (20)
+#define FUZZY_WIDTH (20) //gives some leeway between edge detections
 
-#define THRESHOLD_WIDTH (100)
-#define BAD_CENTER_THRESHOLD (50)
+#define THRESHOLD_WIDTH (100)	 //determines the difference between rising and falling threshold values
+#define BAD_CENTER_THRESHOLD (50) //a threshold for determining erroneous centerline values
 
-class Camera {
-public:
-
-	struct data {
+class Camera
+{
+  public:
+	//where public data about the camera's view will be stored
+	struct data
+	{
 		uint8_t left_edge_inner, left_edge_outer;
 		uint8_t right_edge_inner, right_edge_outer;
 		uint8_t center, prev_center;
 	} camera_data;
 
-	struct calibration {
+	//where calibration values will be stored
+	struct calibration
+	{
 		uint16_t max, min, rising_threshold, falling_threshold;
 	} calibration;
 
-	enum edge_polarity {
-		RISING, FALLING
-	};
-
 	//peripherals responsible for driving the camera
-	FTM_Type* ftm_base;
+	FTM_Type *ftm_base;
 	ftm_chnl_t ftm_channel;
 
-	PIT_Type* pit_base;
+	PIT_Type *pit_base;
 	pit_chnl_t pit_channel;
-	ADC_Type* adc_base;
-	GPIO_Type* gpio_base;
+	ADC_Type *adc_base;
+	GPIO_Type *gpio_base;
 
 	uint32_t clk_pin, si_pin;
 
@@ -59,27 +69,53 @@ public:
 	int16_t current_pixel = 0;
 	uint32_t adc_value = 0;
 
+	//interrupt handler stores pixels here
 	uint16_t line_buffer[NUM_PIXELS];
 
 	void init();
 
-	Camera(FTM_Type* ftm, ftm_chnl_t ftm_chnl, PIT_Type* pit,
-			pit_chnl_t pit_chnl, ADC_Type* adc, GPIO_Type* gpio, uint32_t clk,
-			uint32_t si) :
-			ftm_base { ftm }, ftm_channel { ftm_chnl }, pit_base { pit }, pit_channel {
-					pit_chnl }, adc_base { adc }, gpio_base { gpio }, clk_pin {
-					clk }, si_pin { si }, task_handle { NULL } {
-	}
-	;
+	Camera(FTM_Type *ftm, ftm_chnl_t ftm_chnl, PIT_Type *pit,
+		   pit_chnl_t pit_chnl, ADC_Type *adc, GPIO_Type *gpio, uint32_t clk,
+		   uint32_t si) : ftm_base{ftm}, ftm_channel{ftm_chnl}, pit_base{pit}, pit_channel{
+																				   pit_chnl},
+						  adc_base{adc}, gpio_base{gpio}, clk_pin{clk}, si_pin{si}, task_handle{NULL} {};
 
+	/* this is the main function called by the RTOS camera task.
+	 * it waits for a task notification sent by the interrupt
+	 * handler, and then copies the line of pixel data to a 
+	 * local buffer.
+	 */
 	void process(void);
+
+	/* This function calibrates the rising and falling threshold
+ 	 * values by averaging a number of camera lines while the 
+	 * camera is pointed at the track.
+	 */
 	void calibrate(void);
 
-private:
-	void filter(void);
+  private:
+	//used for the find_edges_between function
+	enum edge_polarity
+	{
+		RISING,
+		FALLING
+	};
+
+	/* this function looks for the black lines from the center
+	 * of the camera line outward in both directions.
+ 	*/
 	void find_edges(uint16_t camline[]);
+
+	/* this function looks for falling or rising threshold crosses between the lower and
+ 	* upper bound values provided. This can work in either direction.
+ 	*/
 	uint8_t find_edge_between(uint8_t lower_bound, uint8_t upper_bound,
-			uint16_t *camline, edge_polarity pol, bool reverse);
+							  uint16_t *camline, edge_polarity pol, bool reverse);
+
+	/* finds a centerline value by averaging the left and right edge positions.
+ 	* It also handles erroneous cases of a centerline being wildly different 
+ 	* from its previous value.
+ 	*/
 	uint8_t calculate_centerline(struct data *camdata);
 };
 
